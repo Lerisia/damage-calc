8c6922983bdb970af7800867c9d08d4d5c1fe3e2
lerisia <rlawodus96@gmail.com>
2026-03-22 17:06:27 +0900

    fix: preload all data before render, IndexedStack for smooth tabs, Tera Blast before skins
    Move name map loading from async DamageCalculatorScreen to AppLoader
    so all data is ready before first frame. Replace TabBarView with
    IndexedStack to eliminate first-tab-switch lag. Reorder Tera Blast
    before skin abilities so Normal Tera Blast can be converted by -ate.

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,8 @@ class _AppLoader extends StatefulWidget {
 
 class _AppLoaderState extends State<_AppLoader> {
   bool _ready = false;
+  Map<String, String> _abilityNameMap = {};
+  Map<String, String> _itemNameMap = {};
 
   @override
   void initState() {
@@ -49,18 +51,43 @@ class _AppLoaderState extends State<_AppLoader> {
   }
 
   Future<void> _preload() async {
-    await Future.wait([
+    final results = await Future.wait([
       loadPokedex(),
       loadAllMoves(),
       loadAbilitydex(),
       loadItemdex(),
     ]);
-    if (mounted) setState(() => _ready = true);
+
+    // Build name maps from loaded data
+    final abilities = results[2] as Map<String, dynamic>;
+    final aMap = <String, String>{};
+    for (final e in abilities.values) {
+      aMap[e.name as String] = e.nameKo as String;
+    }
+
+    final items = results[3] as Map<String, dynamic>;
+    final iMap = <String, String>{};
+    for (final e in items.values) {
+      iMap[e.name as String] = e.nameKo as String;
+    }
+
+    if (mounted) {
+      setState(() {
+        _abilityNameMap = aMap;
+        _itemNameMap = iMap;
+        _ready = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_ready) return const DamageCalculatorScreen();
+    if (_ready) {
+      return DamageCalculatorScreen(
+        abilityNameMap: _abilityNameMap,
+        itemNameMap: _itemNameMap,
+      );
+    }
 
     return Scaffold(
       body: Center(
