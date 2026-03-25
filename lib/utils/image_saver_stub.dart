455d9132375c4429f87276dcde6964e096ebd443
lerisia <rlawodus96@gmail.com>
2026-03-26 00:53:37 +0900

    chore: release signing config, remove unused permissions and packages
    - Configure release signing with upload keystore in build.gradle.kts
    - Remove image_gallery_saver_plus and file_picker packages
    - Remove READ_MEDIA_IMAGES and storage permissions
    - Add feature graphic for Play Store
    - Bump version code to 7

--- lib/utils/image_saver_stub.dart
+++ lib/utils/image_saver_stub.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<bool> saveImage(Uint8List bytes, String filename) async {
-  final result = await ImageGallerySaverPlus.saveImage(
-    bytes,
-    quality: 100,
-    name: filename,
-  );
-  return result != null;
+  final dir = await getApplicationDocumentsDirectory();
+  final file = File('${dir.path}/$filename.png');
+  await file.writeAsBytes(bytes);
+  return true;
 }
 
 Future<bool> saveFile(Uint8List bytes, String filename, {String mimeType = 'application/octet-stream'}) async {
