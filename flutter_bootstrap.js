0814e7ad5a0c79110735ef42dc19bd77125a6df2
lerisia <rlawodus96@gmail.com>
2026-04-07 12:12:08 +0900

    deploy: v1.0.2 update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1389459623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4074466329" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
