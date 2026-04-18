df00b90f051c07777311b1084d2326caad3e1589
lerisia <rlawodus96@gmail.com>
2026-04-19 03:19:13 +0900

    release: v1.3.0 (Simple Mode)

--- lib/views/damage_calculator_screen.dart
+++ lib/views/damage_calculator_screen.dart
@@ -2274,7 +2274,7 @@ class _AboutDialog extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('v1.2.0'),
+          const Text('v1.3.0'),
           const SizedBox(height: 8),
           Text(AppStrings.t('about.description')),
           const SizedBox(height: 8),
