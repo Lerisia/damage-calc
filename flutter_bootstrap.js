e38858e95ca974b4c4a72d5ff24e5f80f7c032b4
lerisia <rlawodus96@gmail.com>
2026-04-20 02:30:14 +0900

    deploy: wide-layout mode toggle uses text label too

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2514785892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2477273717" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
