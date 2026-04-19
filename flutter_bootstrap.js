a3b3e51cc8a1d3fbe9def63271ecdae30d6731b3
lerisia <rlawodus96@gmail.com>
2026-04-20 01:46:21 +0900

    deploy: simple-mode chips no longer auto-pair natures

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3215825968" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3098659501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
