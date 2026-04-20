c5092db13423d34f53923b370d54745d2b9694b5
lerisia <rlawodus96@gmail.com>
2026-04-20 18:45:42 +0900

    deploy: v1.3.9 (+69) restore popupmenu nature picker

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2512692889" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1733899120" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
