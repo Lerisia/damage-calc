1159c539be8912e2eb60b65e443b9b390618ec66
lerisia <rlawodus96@gmail.com>
2026-04-21 13:54:03 +0900

    deploy: v1.4.0 (+73) move filter chip polish

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3450889304" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4164820828" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
