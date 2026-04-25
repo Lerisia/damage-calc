990891bd63dfb46324c6d5f95f0004d16677068b
lerisia <rlawodus96@gmail.com>
2026-04-25 21:41:52 +0900

    deploy: type picker chip stays fixed-size, order shown as corner badge

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3321565019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3807581880" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
