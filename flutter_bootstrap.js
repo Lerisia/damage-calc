53919240967a01eaea530dd32162d26da2db16f4
lerisia <rlawodus96@gmail.com>
2026-04-27 12:28:30 +0900

    deploy: v1.6.0 — team coverage matrix + dex bottom padding

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1647491292" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1230471510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
