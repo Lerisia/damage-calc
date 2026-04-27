1761d127850ada01f97a879f3f5e77942e9d1db0
lerisia <rlawodus96@gmail.com>
2026-04-27 17:27:00 +0900

    deploy: bigger matrix glyphs + drop per-slot RepaintBoundary

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2279008577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1863713880" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
