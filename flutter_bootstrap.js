c46a496ebbdbc76175cc46f961e4a14ba54c16dd
lerisia <rlawodus96@gmail.com>
2026-05-27 12:42:49 +0900

    deploy: 1.9.2+222 — restore default keyboard-resize for search dialog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1431064776" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1298969882" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
