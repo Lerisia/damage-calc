6b604bd56d3ebc18562727d819c7f11b89ebf7dd
lerisia <rlawodus96@gmail.com>
2026-05-28 12:32:47 +0900

    deploy: 1.9.6+255 — EV input focus fix + earlier 1.9.6 changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "98117768" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "505738666" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
