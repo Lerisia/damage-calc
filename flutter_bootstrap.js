7b162df290a82622a94020459b2d7a531f8ec534
lerisia <rlawodus96@gmail.com>
2026-05-29 22:29:39 +0900

    deploy: bulk-translate ~160 stems (94% coverage) + categories

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2597972243" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3543832274" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
