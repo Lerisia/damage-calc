6e4f3582638f8a54db5b6f53cff41422b52e1019
lerisia <rlawodus96@gmail.com>
2026-06-17 17:06:42 +0900

    deploy: Metagross family stats

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
     try { const k = await caches.keys(); await Promise.all(k.map((x)=>caches.delete(x))); } catch (_) {}
     try { await self.registration.unregister(); } catch (_) {}
-    const clients = await self.clients.matchAll({ type: 'window' });
-    for (const c of clients) { try { c.navigate(c.url); } catch (_) {} }
+    const cl = await self.clients.matchAll({ type: 'window' });
+    for (const c of cl) { try { c.navigate(c.url); } catch (_) {} }
   })());
 });
