02dd40671d8bf5beae29862be39a29562437defb
lerisia <rlawodus96@gmail.com>
2026-07-29 20:48:40 +0900

    chore: bump to v1.14.5

--- lib/views/damage_calculator_screen.dart
+++ lib/views/damage_calculator_screen.dart
@@ -2506,7 +2506,7 @@ class AppAboutDialog extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('v1.14.4'),
+          const Text('v1.14.5'),
           const SizedBox(height: 8),
           Text(AppStrings.t('about.description')),
           const SizedBox(height: 8),
